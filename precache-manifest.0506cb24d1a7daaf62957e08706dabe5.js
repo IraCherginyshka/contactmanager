a6900d19563e3e92283b25c42d5bf8ed6da846f8
IraCherginyshka <39628469+IraCherginyshka@users.noreply.github.com>
2019-06-26 23:15:46 +0200

    Updates

---
+++ precache-manifest.0506cb24d1a7daaf62957e08706dabe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cba21ad3d850d3d436501d270cfbd8f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "721b7e5679d33b699098",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d0636a3ec71d679cbdd9",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "721b7e5679d33b699098",
+    "url": "/contactmanager/static/js/2.2dfeb129.chunk.js"
+  },
+  {
+    "revision": "d0636a3ec71d679cbdd9",
+    "url": "/contactmanager/static/js/main.1da67300.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
